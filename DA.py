9fbe65ae87b21ec91f8d0ecea63a72e937aeaca6
A.Ye <xxye2022@gmail.com>
2025-03-14 12:33:32 +0800

    D20250314-1233

--- DA.py
+++ DA.py
@@ -7,5 +7,14 @@ print(dt.head())
 print(dt.info())
 print(dt.describe())
 
-fig = px.bar(dt, x="参与者编号", y="表现均分前后差异", color="分组",title="Wide-Form Input",barmode='group')
+fig = px.bar(dt, x="参与者编号", y="表现均分前后差异", color="分组",title="Wide-Form Input",barmode='group',text_auto=True)
 fig.show()
+
+fig = px.box(dt, x="分组", y="表现均分前后差异")
+fig.show()
+
+fig = px.bar(dt,x="天数",y="全天表现均分",color="分组",title="组间每日表现均分",barmode='',text_auto=True)
+fig.show()
+
+#fig = px.line(dt, x='天数', y='lifeExp', color='country', markers=True)
+#fig.show()
