0beb232384d65ba46dfdfc13cb13d8b96d11c227
A.Ye <xxye2022@gmail.com>
2025-03-16 00:33:38 +0800

    CodeStructure

--- DA.py
+++ DA.py
@@ -1,30 +1,55 @@
 import plotly.express as px
 import pandas as pd
 
+
 dt = pd.read_excel('/Users/alex.y/Documents/Github_Repo/G10_CTB_Global-Finals_Data-Analysis/Base_Data/Excel+ALL+ENG.xlsx')
 
 print(dt.head())
 print(dt.info())
 print(dt.describe())
 
-# Passed 
-
 # Bar + 表现前后均分差异
-#fig = px.bar(dt, x="Participant Number", y="Difference in Mean Performance Scores Pre- and Post-Intervention", color="Group",title="Difference in Mean Performance Scores Pre- and Post-Intervention Between Groups",barmode='group',text="Difference in Mean Performance Scores Pre- and Post-Intervention")
-#fig.update_traces(texttemplate='%{text:.2f}', textposition='outside')
-#fig.update_xaxes(tickmode='linear')
-#fig.show()
+fig = px.bar(
+    dt, 
+    x="Participant Number", 
+    y="Difference in Mean Performance Scores Pre- and Post-Intervention", 
+    color="Group",title="Difference in Mean Performance Scores Pre- and Post-Intervention Between Groups",
+    barmode='group',
+    text="Difference in Mean Performance Scores Pre- and Post-Intervention"
+)
+fig.update_traces(
+    texttemplate='%{text:.2f}', 
+    textposition='outside'
+)
+fig.update_xaxes(
+    tickmode='linear'
+)
+fig.show()
 
 # Boxplot + 表现前后均分差异
-# fig = px.box(dt, x="Group", y="Difference in Mean Performance Scores Pre- and Post-Intervention",title="Difference in Mean Performance Scores Pre- and Post-Intervention Between Groups",points="all")
-# fig.show()
+fig = px.box(
+    dt, 
+    x="Group", 
+    y="Difference in Mean Performance Scores Pre- and Post-Intervention",
+    title="Difference in Mean Performance Scores Pre- and Post-Intervention Between Groups",
+    points="all"
+)
+fig.show()
 
 # Histogram + 组间每日均分对比
-# fig = px.histogram(dt,x="Days",y="Overall Daily Performance Mean Score",color="Group",title="Overall Daily Performance Scores Between Groups",barmode='group',text_auto=".2f",histfunc='avg')
-# fig.show()
+fig = px.histogram(
+    dt,
+    x="Days",
+    y="Overall Daily Performance Mean Score",
+    color="Group",
+    title="Overall Daily Performance Scores Between Groups",
+    barmode='group',
+    text_auto=".2f",
+    histfunc='avg'
+)
+fig.show()
 
-# Pending
-
+# Line
 metrics = [
     "Learning Behaviors and Classroom Adaptation Mean Score",
     "Social Interaction Mean Score",
